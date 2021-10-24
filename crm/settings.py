197901f2ed8f05b5a2b29d6c9f2bacd74ad7400f
ananduv2 <ananduv2.0@gmail.com>
2021-10-24 13:58:54 +0530

    static and media collected

--- crm/settings.py
+++ crm/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 # Default primary key field type
