dee6253e152a87e58b074f130136b04076cc91ac
ananduv2 <ananduv2.0@gmail.com>
2021-11-02 14:05:20 +0530

    added notes model

--- crm/settings.py
+++ crm/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$4%9hw8ef6+5pwh+5_1o0l9gddoe(z0zf%a_cws(f9dlaw)^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
