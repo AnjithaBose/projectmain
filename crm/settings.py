8f7e5b1162b7cd86c71ca4edb1e20341fd92dddd
ananduv2 <ananduv2.0@gmail.com>
2021-11-07 16:57:59 +0530

    data removed

--- crm/settings.py
+++ crm/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$4%9hw8ef6+5pwh+5_1o0l9gddoe(z0zf%a_cws(f9dlaw)^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","13.233.147.246"]
 #ALLOWED_HOSTS = ["*"]
