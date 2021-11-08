e6b50a400e99ac6a53e0bc7c983cf5a312d1b85b
ananduv2 <ananduv2.0@gmail.com>
2021-11-08 08:11:49 +0530

    mail bug fixed

--- crm/settings.py
+++ crm/settings.py
@@ -169,5 +169,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lms.teqstoriesgmail.com'
+EMAIL_HOST_USER = 'lms.teqstories@gmail.com'
 EMAIL_HOST_PASSWORD = 'nshgaespkqxdaodv'
